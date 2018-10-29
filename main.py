611ad4449fa80691e911ab77c751c1a71a1e016b
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2018-10-29 12:03:47 +0530

    add cleanup
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ import sys
 os.system("python get_features_crude.py " + str(sys.argv[1]) + " > output.txt")
 
 # Get the condensed XML file
-os.system("python convert_to_xml.py")
+os.system("python convert_to_xml.py > output.xml")
+
+# Cleanup
+os.system("rm output.txt output.xml")
