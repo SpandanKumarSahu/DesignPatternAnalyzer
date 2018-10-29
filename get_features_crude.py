4659ff2bf9c75b9006cfb79786e128c31ff7f976
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2018-10-29 11:57:17 +0530

    Initial Commit
    1. Use pygccxml to get the features
    2. Convert them to XML format
    
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

---
+++ get_features_crude.py
@@ -0,0 +1,17 @@
+from pygccxml import utils
+from pygccxml import declarations
+from pygccxml import parser
+import pygccxml
+import sys
+
+# Find the location of the xml generator (castxml or gccxml)
+generator_path, generator_name = utils.find_xml_generator()
+
+# Configure the xml generator
+xml_generator_config = parser.xml_generator_configuration_t(
+    xml_generator_path=generator_path,
+    xml_generator=generator_name)
+
+# Parse the c++ file
+decls = parser.parse([sys.argv[1]], xml_generator_config)
+declarations.print_declarations(decls)
---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+# Run pygccxml and get the document
+os.system("python get_features_crude.py " + str(sys.argv[1]) + " > output.txt")
+
+# Get the condensed XML file
+os.system("python convert_to_xml.py")
